ad891a1aed1ab337957725c549c9e84ffb057513
amirhossein <a.amirhosein90@gmail.com>
2019-06-23 00:55:35 +0430

    Design UI My pakkat

--- app/src/main/java/com/example/pakkat/FragmentMe.java
+++ app/src/main/java/com/example/pakkat/FragmentMe.java
@@ -1,6 +1,25 @@
 package com.example.pakkat;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class FragmentMe extends Fragment {
+
+    View view;
+    public FragmentMe() {
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_me, container,false);
+
+        return view;
+    }
 }
