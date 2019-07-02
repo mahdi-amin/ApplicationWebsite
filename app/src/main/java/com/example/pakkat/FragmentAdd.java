67d47d8762a6ce58dd6f5130ad7979b42be5f0df
amirhossein <a.amirhosein90@gmail.com>
2019-07-02 16:49:19 +0430

    ui add image

--- app/src/main/java/com/example/pakkat/FragmentAdd.java
+++ app/src/main/java/com/example/pakkat/FragmentAdd.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -15,6 +16,7 @@ public class FragmentAdd extends Fragment {
 
     public FragmentAdd() {
 
+
     }
 
     @Nullable
@@ -23,6 +25,16 @@ public class FragmentAdd extends Fragment {
 
         view = inflater.inflate(R.layout.fragment_add, container, false);
 
+        ImageView image1 = view.findViewById(R.id.image_1);
+        ImageView image2 = view.findViewById(R.id.image_2);
+        ImageView image3 = view.findViewById(R.id.image_3);
+
+        image1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                
+            }
+        });
         return view;
     }
 }
